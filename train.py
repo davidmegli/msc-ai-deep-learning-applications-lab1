2bdd9de2e56e69d1feba51250b576ced589ea8c2
David Megli <verbatimdvd91@gmail.com>
2025-04-28 00:47:49 +0200

    added a first training function

---
+++ train.py
@@ -0,0 +1,53 @@
+import os
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from utils import load_config, get_model, get_optimizer
+from trainer import Trainer
+
+def main():
+    # Load config
+    config = load_config("config.yaml")
+
+    # Dataset (FakeData per ora, puoi cambiare)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+    train_dataset = datasets.FakeData(transform=transform)
+    val_dataset = datasets.FakeData(transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=config['batch_size'])
+
+    # Model
+    model = get_model(config['model_name'], config['num_classes'])
+
+    # Loss
+    criterion = torch.nn.CrossEntropyLoss()
+
+    # Optimizer
+    optimizer = get_optimizer(config['optimizer'], model.parameters(), config['learning_rate'])
+
+    # Trainer
+    trainer = Trainer(
+        model=model,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        criterion=criterion,
+        optimizer=optimizer,
+        scheduler=None,  # For now
+        device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+        output_dir=config['output_dir'],
+        max_epochs=config['max_epochs'],
+        patience=config['patience'],
+        mixed_precision=config['mixed_precision'],
+        project_name=config['project_name'],
+        use_wandb=config['use_wandb'],
+        run_name=config['run_name'],
+    )
+
+    trainer.train()
+
+if __name__ == "__main__":
+    main()
