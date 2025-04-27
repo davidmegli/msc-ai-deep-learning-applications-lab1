3239530b9bf1e36e136d0bb3f097787191c42d19
David Megli <verbatimdvd91@gmail.com>
2025-04-28 01:22:05 +0200

    edited the correct files

--- train.py
+++ train.py
@@ -3,22 +3,37 @@ Author: David Megli
 Date: 2025-04-28
 '''
 import os
+import argparse
 import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-from utils import load_config, get_model, get_optimizer
+from utils import load_config, get_model, get_optimizer, get_loss
 from trainer import Trainer
+from datetime import datetime
+import yaml
 
 def main():
-    # Load config
-    config = load_config("config_mlp.yaml")  # Load config MLP
+    # Argument parser per la configurazione
+    parser = argparse.ArgumentParser(description="Training Script")
+    parser.add_argument('--config', type=str, default="config_mlp.yaml", help="Path to config file.")
+    args = parser.parse_args()
 
-    # Setup dataset e transform
+    # Load config
+    config = load_config(args.config)
+
+    # Save config usato nella output dir
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    os.makedirs(config['output_dir'], exist_ok=True)
+    config_save_path = os.path.join(config['output_dir'], f"used_config_{timestamp}.yaml")
+    with open(config_save_path, 'w') as f:
+        yaml.dump(config, f)
+
+    # Setup dataset
     if config['model_name'].lower() == "simple_mlp":
         transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Lambda(lambda x: x.view(-1))  # Flatten MNIST (28*28)
+            transforms.Lambda(lambda x: x.view(-1))  # Flatten
         ])
         train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
         val_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
@@ -41,10 +56,15 @@ def main():
     model = get_model(config['model_name'], config['num_classes'], config.get('model_params', {}))
 
     # Loss
-    criterion = torch.nn.CrossEntropyLoss()
+    criterion = get_loss(config['loss'])
 
     # Optimizer
     optimizer = get_optimizer(config['optimizer'], model.parameters(), config['learning_rate'])
+
+    # Scheduler (se richiesto)
+    scheduler = None
+    if config.get('use_scheduler', False):
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=config.get('scheduler_step', 10), gamma=config.get('scheduler_gamma', 0.1))
 
     # Trainer
     trainer = Trainer(
@@ -53,7 +73,7 @@ def main():
         val_loader=val_loader,
         criterion=criterion,
         optimizer=optimizer,
-        scheduler=None,
+        scheduler=scheduler,
         device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
         output_dir=config['output_dir'],
         max_epochs=config['max_epochs'],
