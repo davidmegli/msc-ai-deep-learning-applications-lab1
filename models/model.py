434bc98ea508cc73cf95908239919c01bb0535e2
David Megli <verbatimdvd91@gmail.com>
2025-04-28 12:05:34 +0200

    added parametrized mlp

--- models/model.py
+++ models/model.py
@@ -16,4 +16,18 @@ class SimpleMLP(nn.Module):
         )
 
     def forward(self, x):
-        return self.net(x.view(x.size(0), -1))  # Flatten
+        return self.net(x.view(x.size(0), -1))  # Flatten 👍
+    
+
+class ParametrizedMLP(nn.Module):
+    def __init__(self, layer_sizes):
+        super(ParametrizedMLP, self).__init__()
+        layers = []
+        for i in range(len(layer_sizes) -1):
+            layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
+            if i < len(layer_sizes) - 2:
+                layers.append(nn.ReLU())
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net(x.view(x.size(0), -1))
